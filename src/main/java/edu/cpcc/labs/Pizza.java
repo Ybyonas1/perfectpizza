b171cb8e44255288203468b8677865aacd45a00f
Yonas Woldemichael <ybyonas@gmail.com>
2024-10-16 20:12:35 -0400

    initial release.

---
+++ src/main/java/edu/cpcc/labs/Pizza.java
@@ -0,0 +1,15 @@
+package edu.cpcc.labs;
+
+public class Pizza {
+	
+	private PizzaType pizzaType;
+
+	public PizzaType getPizzaType() {
+		return pizzaType;
+	}
+
+	public void setPizzaType(PizzaType pizzaType) {
+		this.pizzaType = pizzaType;
+	}
+
+}
